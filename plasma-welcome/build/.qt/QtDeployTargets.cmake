002a62c838b052b72a44dc36d24bf84f267868da
Alexandru Balan <alxb421@gmail.com>
2026-02-09 01:04:34 +0200

    add Liquid Gel fix

--- plasma-welcome/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_pearos-welcome_FILE /home/alxb421/Desktop/pkgbuilds/plasma-welcome/build/pearos-welcome)
-set(__QT_DEPLOY_TARGET_pearos-welcome_TYPE EXECUTABLE)
--- plasma-welcome/src/plasma-welcome/build/CMakeFiles/pearos-welcome.dir/pearos-welcome_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pearos-welcome.dir/pearos-welcome_autogen/mocs_compilation.cpp.o: \
- /home/alxb421/Desktop/pkgbuilds/plasma-welcome/src/plasma-welcome/build/pearos-welcome_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
