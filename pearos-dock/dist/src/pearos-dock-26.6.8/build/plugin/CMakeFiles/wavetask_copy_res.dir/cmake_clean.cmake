8373c4861ce71f9cf69d25409eecc31fff30391c
Alexandru Balan <alxb421@gmail.com>
2026-07-24 19:27:04 +0300

    26.7

--- pearos-dock/dist/src/pearos-dock-26.6.8/build/plugin/CMakeFiles/wavetask_copy_res.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/wavetask_res.txt"
-  "CMakeFiles/wavetask_copy_res"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/wavetask_copy_res.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- pearos-dock/dist/src/pearos-dock-26.6.8/build/plugin/CMakeFiles/wavetaskplugin.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for wavetaskplugin.
-# This may be replaced when dependencies are built.
