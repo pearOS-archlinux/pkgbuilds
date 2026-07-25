88b275b4d7484b71626dd7c4948c0bf3dc90d439
Alexandru Balan <alxb421@gmail.com>
2026-07-25 10:06:57 +0300

    fix lag & pearID

--- pearos-notes/src/gui/mainwindow.h
+++ pearos-notes/src/gui/mainwindow.h
@@ -16,6 +16,7 @@
 #include "../utils/types.h"
 
 class NotesWidget;
+class PearIDManager;
 
 /**
  * @brief Main application window - Frameless, transparent with blur (Classmorphism).
@@ -40,6 +41,7 @@ private:
     void setupSidebar();
     void setupContent();
     void loadPearIdDisplayName();
+    void applyPearIdState();
     void createMenuBar();
     void loadStyleSheet();
     QString themeOverlayFromPalette() const;
@@ -64,6 +66,8 @@ private:
     QListWidget* m_sideList = nullptr;
     QWidget* m_sidebarProfileWidget = nullptr;
     QLabel* m_sidebarProfileName = nullptr;
+    QLabel* m_sidebarProfileImg = nullptr;
+    PearIDManager* m_pearId = nullptr;
 
     NotesWidget* m_notesWidget = nullptr;
     QVector<NoteDocument> m_notes;
